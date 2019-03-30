82df567aa0ccb52e6cfc8454157f11d79834700a
limi-son <1464398816@qq.com>
2019-03-30 15:12:48 +0800

    搭建路由

---
+++ houtai/src/pages/OrderList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let OrderList = ()=>{
+    return <div  className="conts" >
+        订单列表
+    </div>
+}
+
+export default OrderList;
---
+++ houtai/src/pages/UserList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let UserList = ()=>{
+    return <div  className="conts" >
+        用户列表
+    </div>
+}
+
+export default UserList;
